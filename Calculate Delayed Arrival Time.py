76019629f5e4413ec4e6fff64b1139eb8979b4d5
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2024-03-05 00:37:30 +0530

    Update Calculate Delayed Arrival Time.py

--- Calculate Delayed Arrival Time.py
+++ Calculate Delayed Arrival Time.py
@@ -1,4 +1,4 @@
 class Solution:
     def findDelayedArrivalTime(self, arrivalTime: int, delayedTime: int) -> int:
-        return (arrivalTime+delayedTime)%24
+        return (arrivalTime+delayedTime) % 24
         

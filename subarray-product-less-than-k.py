535eb868cdcb10fa2fdaf5052bf54736f25c596b
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2024-03-27 22:27:35 +0530

    Create subarray-product-less-than-k.py

---
+++ subarray-product-less-than-k.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        if k <= 1:
+            return 0
+            
+        i, j = 0, 0
+        count = 0
+        product = 1
+        while j < len(nums):
+            product *= nums[j]
+            while product >= k:
+                product /= nums[i]
+                i += 1
+            count += j - i + 1
+            j += 1
+        return count

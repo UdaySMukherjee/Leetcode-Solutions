56c2e9d3d836b5319c715d7a81d07310101872b5
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2024-03-31 20:19:32 +0530

    Create count-subarrays-with-fixed-bounds.py
    We can approach this problem using a sliding window technique. We iterate through the list nums and maintain three indices: bad_idx, left_idx, and right_idx, representing the indices of the last encountered elements violating the condition, the last encountered minK, and the last encountered maxK, respectively. As we iterate, we update these indices accordingly. At each step, we calculate the number of valid subarrays ending at the current index and add it to the result.

---
+++ count-subarrays-with-fixed-bounds.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
+        
+        res = 0
+        bad_idx = -1
+        left_idx = -1
+        right_idx = -1
+
+        for i, num in enumerate(nums) :
+            if not (minK <= num and num <= maxK):
+                bad_idx = i
+
+            if num == minK:
+                left_idx = i
+
+            if num == maxK:
+                right_idx = i
+
+            res += max(0, min(left_idx, right_idx) - bad_idx)
+
+        return res

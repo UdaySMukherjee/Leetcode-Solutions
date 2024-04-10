770fcbf26a7ed1f2bcc06189c8dd2d5bb3dd4a9e
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2024-04-10 08:40:19 +0530

    Create reveal-cards-in-increasing-order.py
    1. First, we sort the deck in descending order since we need to start with the largest card.
    2. We then simulate the revealing process by using a deque.
    3. We start with the largest card (first after sorting) and place it at the bottom of the deque.
    4. For each subsequent card, we take the top card from the deque, move it to the bottom, and then place the current card on top of the deque.
    5. Finally, we retrieve the elements from the deque, which now represents the order in which cards are revealed.

---
+++ reveal-cards-in-increasing-order.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+class Solution:
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        deck.sort(reverse=True)
+        dq = deque()
+        n = len(deck)
+        dq.appendleft(deck[0])
+        for i in range(1, n):
+            x = dq.pop()
+            dq.appendleft(x)
+            dq.appendleft(deck[i])
+
+        ans = []
+        while dq:
+            ans.append(dq.popleft())
+        return ans

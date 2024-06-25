a4acb92c7446533ba4bacb069f485396d9fa50ac
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2024-06-25 09:36:08 +0530

    Create roman-to-integer.py
    1. Mapping Values: Define a dictionary to map Roman numerals to integers.
    2. Replacing Subtractive Combinations: Use string replacements to convert special combinations
    to simpler forms:
    - Replace "IV" with "IIII"
    - Replace "IX" with "VIIII"
    - Replace "XL" with "XXXX"
    - Replace "XC" with "LXXXX"
    - Replace "CD" with "CCCC'
    - Replace "CW with "DCCCC"
    3. Summing the Values: Iterate through the modified string, look up each character in the
    dictionary, and accumulate the total sum.

---
+++ roman-to-integer.py
@@ -0,0 +1,10 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        values = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000 }
+        sum = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for i in range(len(s)):
+            sum += values[s[i]]
+        return sum

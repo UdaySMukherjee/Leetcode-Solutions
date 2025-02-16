8db14fe5ab48fce20bc31d4887f46a419ba6c26b
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2025-02-16 12:50:26 +0530

    Create construct-the-lexicographically-largest-valid-sequence.py

---
+++ construct-the-lexicographically-largest-valid-sequence.py
@@ -0,0 +1,29 @@
+class Solution:
+  def constructDistancedSequence(self, n: int) -> list[int]:
+    ans = [0] * (2 * n - 1)
+    def dfs(i: int, mask: int) -> bool:
+      if i == len(ans):
+        return True
+      if ans[i] > 0:
+        return dfs(i + 1, mask)
+      for num in range(n, 0, -1):
+        if (mask >> num & 1) == 1:
+          continue
+        if num == 1:
+          ans[i] = num
+          if dfs(i + 1, mask | 1 << num):
+            return True
+          ans[i] = 0
+        else:  
+          if i + num >= len(ans) or ans[i + num] > 0:
+            continue
+          ans[i] = num
+          ans[i + num] = num
+          if dfs(i + 1, mask | 1 << num):
+            return True
+          ans[i + num] = 0
+          ans[i] = 0
+
+      return False
+    dfs(0, 0)
+    return ans

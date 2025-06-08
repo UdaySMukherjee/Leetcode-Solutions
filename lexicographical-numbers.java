1f52d2694b500d83f7847ffa5a36b6303b208ce3
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2025-06-08 13:26:07 +0530

    Create lexicographical-numbers.java

---
+++ lexicographical-numbers.java
@@ -0,0 +1,16 @@
+class Solution {
+    static public List<Integer> lexicalOrder(int n) {
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 1; i < 10; i++) 
+            solve(i, n, ans);
+        return ans;
+    }
+
+    static void solve(int prod, int n, List<Integer> ans) {
+        if (prod > n) return;
+        ans.add(prod);
+        for (int j = 0; j < 10; j++) {
+            solve(prod * 10 + j, n, ans);
+        }
+    }
+}

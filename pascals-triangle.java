6d043900a8f025ff0c6539eb8323c4c0146a57b6
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2025-08-01 15:46:48 +0530

    Create pascals-triangle.java

---
+++ pascals-triangle.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            row.add(1);
+
+            if (i > 0) {
+                List<Integer> prevRow = triangle.get(i - 1);
+                for (int j = 1; j < i; j++) {
+                    row.add(prevRow.get(j - 1) + prevRow.get(j));
+                }
+                row.add(1);
+            }
+
+            triangle.add(row);
+        }
+        return triangle;
+    }
+}

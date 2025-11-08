a429f04c32fff2b4eb53bb5543c048166f43f48b
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2025-11-08 13:30:06 +0530

    Create minimum-one-bit-operations-to-make-integers-zero.java

---
+++ minimum-one-bit-operations-to-make-integers-zero.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minimumOneBitOperations(int n) {
+        int result = 0;
+        while (n > 0) {
+            result ^= n;
+            n = n >> 1;
+        }
+        
+        return result;
+    }
+}

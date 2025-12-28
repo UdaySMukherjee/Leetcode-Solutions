f9d36f207c6fef929edb9ce61d442d19f05dbdc3
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2025-12-28 09:55:35 +0530

    Create count-negative-numbers-in-a-sorted-matrix.java

---
+++ count-negative-numbers-in-a-sorted-matrix.java
@@ -0,0 +1,17 @@
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+
+        r = 0
+        c = cols - 1
+        count = 0
+
+        while r < rows and c >= 0:
+            if grid[r][c] < 0:
+                count += (rows - r)
+                c -= 1
+            else:
+                r += 1
+
+        return count

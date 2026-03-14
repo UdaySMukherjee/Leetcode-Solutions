aeac58a70ee3996efa6575ca3acd2477ebee03aa
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2026-03-14 21:09:31 +0530

    Update 1-bit-and-2-bit-characters.java

--- 1-bit-and-2-bit-characters.java
+++ 1-bit-and-2-bit-characters.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isOneBitCharacter(int[] bits) {
+        //
         for(int i = 0; i < bits.length; i++) {
             if(i == bits.length - 1) return true;
             if(bits[i] == 1) i++;

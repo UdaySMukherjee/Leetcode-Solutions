b1a131df7e512dc9cc3f9f85ffbe86745f2f1307
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2026-04-25 12:19:12 +0530

    Update maximize-the-distance-between-points-on-a-square.py

--- maximize-the-distance-between-points-on-a-square.py
+++ maximize-the-distance-between-points-on-a-square.py
@@ -1,34 +1,45 @@
 class Solution:
-    def maxDistance(self, side: int, points: List[List[int]], k: int) -> int:
+    def maxDistance(self, side: int, points: list[list[int]], k: int) -> int:
+        a = []
+        for x, y in points:
+            if x == 0:
+                a.append(y)
+            elif y == side:
+                a.append(side + x)
+            elif x == side:
+                a.append(side * 3 - y)
+            else:
+                a.append(side * 4 - x)
+        a.sort()
 
-        def flatten(p: list) -> int:            # flattens points to a 1D array
-            x, y = p
-            if y == 0   : return x              # bottom
-            if x == side: return side + y       # right
-            if y == side: return 3 * side  - x  # top
-            if x == 0   : return 4 * side  - y  # left
-
-        def notValid(mnDist: int) -> bool:      # finds whether dist fails
-            for i, num in enumerate(arr):
-                ptr, cnt = i, 1
-                
-                while cnt < k:
-                    j = bisect_left(arr, arr[ptr] + mnDist)
-                    if j == len(points): break
-
-                    ptr = j
-                    cnt += 1
-
-                    if mnDist + arr[ptr] > num + 4 * side:
-                        cnt = 0
-                        break
-
-                if cnt == k:
+        def check(low: int) -> bool:
+            idx = [0] * k
+            cur = a[0]
+            for j in range(1, k):
+                i = bisect_left(a, cur + low)
+                if i == len(a):
                     return False
+                idx[j] = i
+                cur = a[i]
+            if cur - a[0] <= side * 4 - low:
+                return True
 
-            return True
+            # 第一个指针移动到第二个指针的位置，就不用继续枚举了
+            for idx[0] in range(1, idx[1]):
+                for j in range(1, k):
+                    while a[idx[j]] < a[idx[j - 1]] + low:
+                        idx[j] += 1
+                        if idx[j] == len(a):
+                            return False
+                if a[idx[-1]] - a[idx[0]] <= side * 4 - low:
+                    return True
+            return False
 
-        arr = sorted(map(flatten, points))
-        firstFalse = bisect_left(range(0, side + 1), True, key = notValid)
-        return firstFalse - 1           # the max min is one less than 
-                                        # the min failure
+        left, right = 1, side + 1
+        while left + 1 < right:
+            mid = (left + right) // 2
+            if check(mid):
+                left = mid
+            else:
+                right = mid
+        return left

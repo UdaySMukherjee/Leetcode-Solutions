43f57846b27a7592b5fed06b416562b70f39500c
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2026-08-23 17:00:31 +0530

    Create count-different-palindromic-subsequences.py

---
+++ count-different-palindromic-subsequences.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countPalindromicSubsequences(self, s: str) -> int:
+
+        @lru_cache(None)
+        def dp(left: int, rght: int, res = 0):
+
+            for ch in 'abcd':
+
+                l, r = s.find(ch,left,rght), s.rfind(ch,left,rght)
+                
+                res+= 0 if l==-1 else 1 if l==r else dp(l+1,r)+2
+
+            return res %1_000_000_007
+
+        return dp(0,len(s))

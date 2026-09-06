f9d695096ab74da6ecea46a13dfb5b187fca03e2
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2026-09-06 07:46:20 +0530

    Create distinct-subsequences.py

---
+++ distinct-subsequences.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        m, n=len(s), len(t)
+        dp=[0]*(n+1)
+        dp[0]=1
+        for i in range(1, m+1):
+            for j in range(min(i, n), 0, -1):
+                if s[i-1]==t[j-1]: dp[j]+=dp[j-1]
+        return dp[n]       
